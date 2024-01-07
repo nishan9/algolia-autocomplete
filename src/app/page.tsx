14794b979643e89bfe1f7f4b0b7c18766702b4c9
Nishan <nishan9@icloud.com>
2024-01-07 23:00:14 +0000

    added css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { getAlgoliaResults } from "@algolia/autocomplete-js";
 import algoliasearch from "algoliasearch";
 import { Autocomplete } from "./components/Autocomplete";
 import { ProductItem } from "./components/ProductItem";
+import "@algolia/autocomplete-theme-classic";
 
 const appId = "E1TXPKYP9U";
 const apiKey = "558b522985ea1f3a603fdb2196da443c";
@@ -12,31 +13,35 @@ const searchClient = algoliasearch(appId, apiKey);
 
 function Page() {
   return (
-    <Autocomplete
-      openOnFocus={true}
-      getSources={({ query }) => [
-        {
-          sourceId: "products",
-          getItems() {
-            return getAlgoliaResults({
-              searchClient,
-              queries: [
-                {
-                  indexName:
-                    "netlify_aa5cd1dd-815c-44fc-908a-67f4ec2ba3b8_master_all",
-                  query,
+    <div className="bg-yellow-400 flex flex-col h-screen justify-center items-center">
+      <div className="w-1/2">
+        <Autocomplete
+          openOnFocus={true}
+          getSources={({ query }) => [
+            {
+              sourceId: "products",
+              getItems() {
+                return getAlgoliaResults({
+                  searchClient,
+                  queries: [
+                    {
+                      indexName:
+                        "netlify_aa5cd1dd-815c-44fc-908a-67f4ec2ba3b8_master_all",
+                      query,
+                    },
+                  ],
+                });
+              },
+              templates: {
+                item({ item, components }) {
+                  return <ProductItem hit={item} components={components} />;
                 },
-              ],
-            });
-          },
-          templates: {
-            item({ item, components }) {
-              return <ProductItem hit={item} components={components} />;
+              },
             },
-          },
-        },
-      ]}
-    />
+          ]}
+        />
+      </div>
+    </div>
   );
 }
 
