bd296cfad2a783904112eaf772e060f2189a56bb
Nishan <nishan9@icloud.com>
2024-01-07 10:36:58 +0000

    example demo without typescript

---
+++ src/app/components/ProductItem.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React, { createElement } from "react";
+
+export function ProductItem({ hit, components }: any) {
+  return (
+    <a href={hit.url} className="aa-ItemLink">
+      <div className="aa-ItemContent">
+        <div className="aa-ItemTitle">
+          <components.Highlight hit={hit} attribute="name" />
+        </div>
+      </div>
+    </a>
+  );
+}
